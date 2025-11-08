9c34b4b7ba21648871f8d2c8b24bf723f2ad82a9
taikurachi <tk2958@nyu.edu>
2025-11-08 18:21:59 -0500

    creating reusable credit card component

---
+++ app/analysis/components/CreditCard.tsx
@@ -0,0 +1,9 @@
+import { CreditCard } from "@/lib/types";
+
+interface CreditCardProps {
+  cards: CreditCard[][];
+}
+
+export default function CreditCard({ cards, status }: CreditCardProps) {
+  return <div></div>;
+}
